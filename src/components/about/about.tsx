eadf2b585b255b9a069385ecaec039c697cff70b
Haider Ali <143487236+HaiderAli170@users.noreply.github.com>
2024-03-23 15:23:31 +0500

    updated

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,5 +1,5 @@
-import programer from '../../assets/images/programer.png'
-import tag from '../../assets/images/tag.png'
+import programer from "../../assets/images/programer.png";
+import tag from "../../assets/images/tag.png";
 
 const About = () => {
   return (
@@ -9,32 +9,62 @@ const About = () => {
           <div className="relative">
             <div className="absolute top-0 -left-44 z-0 opacity-50">
               {/* Use imported image or direct URL */}
-              <img src={tag} className="w-36 z-0 h-full object-fill" alt="Placeholder" />
+              <img
+                src={tag}
+                className="w-36 z-0 h-full object-fill"
+                alt="Placeholder"
+              />
             </div>
             <div className="lg:max-w-xl lg:pr-5 relative z-40">
-            <h1 className=" cursor-pointer tracking-wide hover:decoration-sky-600 font-medium text-4xl text-white font-poppin rounded rounder-lg animate-pulse">
-        About
-      </h1>
-              <h2 className="mb-6 max-w-lg text-5xl text-white font-light leading-snug tracking-tight text-g1 sm:text-7xl sm:leading-snug">
-                We make you look
-                <span className="my-1 inline-block border-b-8 border-g4 bg-white px-4 font-bold text-g4 animate__animated animate__flash">different</span>
+              <h1 className=" cursor-pointer tracking-wide hover:decoration-sky-600 font-medium text-4xl text-white font-poppin rounded rounder-lg animate-pulse">
+                About
+              </h1>
+              <h2 className="mb-6 max-w-lg text-5xl text-white font-great-vibes leading-snug tracking-tight text-g1 sm:text-7xl sm:leading-snug">
+                i am <span className="underline text-blue-400  decoration-slate-600 cursor-pointer"> React</span> a
+                <span className="my-1 text-gray-300 inline-block border-b-8 border-gray-900 bg-next px-4 font-bold text-g4 animate__animated animate__flash">
+                  Developer
+                </span>
               </h2>
-              <p className="text-base text-gray-700">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque it.</p>
+              <p className="text-base font-poppin tracking-wide text-gray-300">
+              As a passionate Software Engineer, I specialize in crafting innovative solutions to complex problems. With a robust foundation in both front-end and back-end technologies, my portfolio showcases a range of projects from dynamic web applications to efficient, scalable backend systems. My dedication to continuous learning and improvement is reflected in my work, which leverages the latest technologies and methodologies to deliver high-quality software. I'm committed to using my technical skills and creativity to develop software that not only meets, but exceeds expectations.
+              </p>
               <div className="mt-10 flex flex-col items-center md:flex-row">
-                <a href="/" className="mb-3 inline-flex h-12 w-full items-center justify-center rounded bg-green-600 px-6 font-medium tracking-wide text-white shadow-md transition hover:bg-blue-800 focus:outline-none md:mr-4 md:mb-0 md:w-auto">
+                <a
+                  href="/"
+                  className="mb-3 inline-flex h-12 w-full items-center justify-center rounded bg-green-600 px-6 font-medium tracking-wide text-white shadow-md transition hover:bg-blue-800 focus:outline-none md:mr-4 md:mb-0 md:w-auto"
+                >
                   View More
                 </a>
-                <a href="/" aria-label="Watch how it works" className="group inline-flex items-center font-semibold text-g1">
+                <a
+                  href="/"
+                  aria-label="Watch how it works "
+                  className="group inline-flex bg-slate-600 rounded-lg p-2 items-center font-semibold text-g1"
+                >
                   Watch how it works
-                  <svg xmlns="http://www.w3.org/2000/svg" className="ml-4 h-6 w-6 transition-transform group-hover:translate-x-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="ml-4 h-6 w-6 transition-transform group-hover:translate-x-2"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17 8l4 4m0 0l-4 4m4-4H3"
+                    ></path>
                   </svg>
                 </a>
               </div>
             </div>
           </div>
           <div className="relative   lg:ml-32 lg:block lg:w-1/2">
-            <img src={programer} alt="Showcase" className="abg-orange-400 shadow-2xl shadow-slate-700 opacity-75  mx-auto w-fit overflow-hidden rounded-[22rem] rounded-br-none rounded-tl-none"/>
+            <img
+              src={programer}
+              alt="Showcase"
+              className="abg-orange-400 shadow-2xl shadow-slate-700 opacity-75  mx-auto w-fit overflow-hidden rounded-[22rem] rounded-br-none rounded-tl-none"
+            />
           </div>
         </div>
       </div>
