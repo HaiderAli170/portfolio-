ab42cd82efb3e5c4bd144c06e778a9e790f1811d
Haider Ali <143487236+HaiderAli170@users.noreply.github.com>
2024-03-23 15:36:40 +0500

    updated

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -6,8 +6,8 @@ const About = () => {
     <div className="bg-black flex items-center justify-center overflow-hidden z-50">
       <div className="relative mx-auto h-full px-4 pb-20 md:pb-10 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
         <div className="flex flex-col items-center justify-between lg:flex-row py-16">
-          <div className="relative">
-            <div className="absolute top-0 -left-44 z-0 opacity-50">
+          <div className="relative ">
+            <div className="absolute top-0 -left-7  z-0 opacity-50">
               {/* Use imported image or direct URL */}
               <img
                 src={tag}
@@ -15,7 +15,7 @@ const About = () => {
                 alt="Placeholder"
               />
             </div>
-            <div className="lg:max-w-xl lg:pr-5 relative z-40">
+            <div className="lg:max-w-xl lg:pr-5 mt-4 relative z-40">
               <h1 className=" cursor-pointer tracking-wide hover:decoration-sky-600 font-medium text-4xl text-white font-poppin rounded rounder-lg animate-pulse">
                 About
               </h1>
@@ -30,32 +30,13 @@ const About = () => {
               </p>
               <div className="mt-10 flex flex-col items-center md:flex-row">
                 <a
-                  href="/"
+                  href="/src/assets/images/Haider Ali Resume (1).pdf"
+                  download="Haider_Ali_Resume.pdf" 
                   className="mb-3 inline-flex h-12 w-full items-center justify-center rounded bg-green-600 px-6 font-medium tracking-wide text-white shadow-md transition hover:bg-blue-800 focus:outline-none md:mr-4 md:mb-0 md:w-auto"
                 >
-                  View More
+                  Download Resume
                 </a>
-                <a
-                  href="/"
-                  aria-label="Watch how it works "
-                  className="group inline-flex bg-slate-600 rounded-lg p-2 items-center font-semibold text-g1"
-                >
-                  Watch how it works
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="ml-4 h-6 w-6 transition-transform group-hover:translate-x-2"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M17 8l4 4m0 0l-4 4m4-4H3"
-                    ></path>
-                  </svg>
-                </a>
+               
               </div>
             </div>
           </div>
